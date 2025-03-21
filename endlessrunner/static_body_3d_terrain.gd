7b4ed21378c0c38c5a4d6166c4086e18fedf5036
milaanastasova <anastasova.mila@gmail.com>
2025-03-21 20:51:43 +0100

    initial commit

---
+++ endlessrunner/static_body_3d_terrain.gd
@@ -0,0 +1,12 @@
+extends StaticBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	add_to_group("terrain")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
